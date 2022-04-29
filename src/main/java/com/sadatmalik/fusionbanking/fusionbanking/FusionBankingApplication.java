d08463b767638eae0487ba689b17f4eae084fcc1
sadat.malik <sadat.malik@me.com>
2022-04-29 11:19:16 +0100

    initial spring.io project files

---
+++ src/main/java/com/sadatmalik/fusionbanking/fusionbanking/FusionBankingApplication.java
@@ -0,0 +1,18 @@
+package com.sadatmalik.fusionbanking.fusionbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring bootstrap application. Launches Fusion Banking.
+ *
+ * @author sadatmalik
+ */
+@SpringBootApplication
+public class FusionBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadatmalik/fusionbanking/fusionbanking/FusionBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.fusionbanking.fusionbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusionBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
